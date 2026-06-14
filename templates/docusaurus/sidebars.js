7dfd3a5b06e82c206afb2e30f55833c3f5d7d1b9
cyberdocs120 <cyberdocs120@gmail.com>
2026-06-14 19:28:13 +0100

    feat(templates): add Docusaurus site templates and OpenAPI base spec

---
+++ templates/docusaurus/sidebars.js
@@ -0,0 +1,25 @@
+module.exports = {
+  docs: [
+    'overview',
+    {
+      type: 'category',
+      label: 'Functions',
+      items: ['functions/PLACEHOLDER_FUNCTIONS'],
+    },
+    {
+      type: 'category',
+      label: 'Events',
+      items: ['events/PLACEHOLDER_EVENTS'],
+    },
+    {
+      type: 'category',
+      label: 'Errors',
+      items: ['errors/error-reference'],
+    },
+    {
+      type: 'category',
+      label: 'SDK',
+      items: ['sdk/quickstart'],
+    },
+  ],
+};
